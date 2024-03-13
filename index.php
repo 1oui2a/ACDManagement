450edca5cc988a2acdfcdc029f3a79395a8d979d
Louiza Moran <g0039496@atu.ie>
2024-03-13 16:01:08 +0000

    website

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>My Page</title>
+  </head>
+  <body>
+    <h1>This is a HTML header!</h1>
+    <?php
+      echo "This is some php text!";
+    ?>
+    <!-- Uncomment the line below once you have the shopping cart integrated -->
+    <!-- <a href="shop.html" class="button">Go to Shop</a> -->
+  </body>
+</html>
