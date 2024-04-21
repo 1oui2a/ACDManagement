f1a40fff2d1059e7f6270207cf4f2d5ce12908cc
Louiza Moran <g0039496@atu.ie>
2024-04-21 00:59:22 +0000

    editing# Please enter the commit message for your changes. Lines starting

--- databaseACD.sql
+++ databaseACD.sql
@@ -1,5 +1,5 @@
 -- Set storage engine to InnoDB
-SET default_storage_engine=InnoDB;
+/*SET default_storage_engine=InnoDB;
 -- Delete database if it currently exists
 DROP DATABASE IF EXISTS databaseACD;
 -- Create database with utf8mb4 character set and utf8mb4_unicode_ci collation
