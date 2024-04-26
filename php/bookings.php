52016406f85d16b5d720964001b65a5b40dc0a79
Louiza Moran <g0039496@atu.ie>
2024-04-26 13:45:01 +0000

    mail composer installed

--- php/bookings.php
+++ php/bookings.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
  function db_connect(){
      $servername = "localhost";
      $username = "root";
@@ -21,6 +25,8 @@
         return $result;
     }
 
+    $conn = db_connect();
+
 // Get data from form
 $firstName = $_POST["first_name"];
 $lastName = $_POST["last_name"];
@@ -29,16 +35,28 @@ $date = $_POST["date"];
 $time = $_POST["time"];
 
 // Prepare and bind for security to prevent injection attacks
-$stmt = $conn->prepare("INSERT INTO bookings (first_name, last_name, email, date, time) VALUES (?, ?, ?, ?, ?)");
-$stmt->bind_param("sssss", $firstName, $lastName, $email, $date, $time);
+$stmt = $conn->prepare("INSERT INTO bookings (first_name, last_name, email, date, time) VALUES (?, ?, ?, ?, ?)"); // ? are placeholders for data to be inserted
+$stmt->bind_param("sssss", $firstName, $lastName, $email, $date, $time); // s means string data type for each parameter to be inserted into the database
 
 // Execute the prepared statement
-if ($stmt->execute()) {
+if ($stmt->execute()) { // execute the statement
     echo "New record created successfully";
+       // Send an email
+       $to = "louizamoran@gmail.com";
+       $subject = "New booking";
+       $message = "New booking from $first_name $last_name on $date at $time.";
+       $headers = "From: webmaster@example.com";
+   
+       if (mail($to, $subject, $message, $headers)) {
+           echo "Email sent successfully";
+       } else {
+           echo "Error sending email";
+         }
 } else {
     echo "Error: " . $stmt->error;
 }
 
 $stmt->close();
 $conn->close();
+mysqli_close($conn);
 ?>
--- php/email.php
+++ php/email.php
@@ -1,18 +1,51 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require '../vendor/autoload.php';
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $firstName = $_POST["first_name"];
-    $lastName = $_POST["last_name"];
+    $first_name = $_POST["first_name"];
+    $last_name = $_POST["last_name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
 
-    $to = "louizamoran1@yahoo.ie";
-    $subject = "New message from $first_name $last_name";
-    $headers = "From: $email";
+    $mail = new PHPMailer(true);
 
-    if (mail($to, $subject, $message, $headers)) {
-        echo "Email sent successfully";
-    } else {
-        echo "Email sending failed";
+
+    $mail = new PHPMailer(true);
+    
+    try {
+        //Server settings
+        $mail->isSMTP();                                      
+        $mail->Host       = 'smtp.gmail.com';  
+        $mail->SMTPAuth   = true;                             
+        $mail->Username   = 'louizamoran@gmail.com';          
+        $mail->Password = getenv('GMAIL_PASSWORD');        
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;                            
+        $mail->Port       = 587;                             
+    
+        //Rest of your code...
+                           
+
+        //Recipients
+        $mail->setFrom($email, "$first_name $last_name");
+        $mail->addAddress('louizamoran1@gmail.com');     
+
+        // Content
+        $mail->isHTML(true);                                  
+        $mail->Subject = "New message from $first_name $last_name";
+        $mail->Body    = $message;
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
 }
 ?>
